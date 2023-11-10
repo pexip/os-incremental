a72421daa660f9ed27176a4b039188ee9e69db64
Vincent Sanders <vince@pexip.com>
2023-11-10 11:26:19 +0000

    Import Upstream version 21.3.0

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@
 
 from __future__ import absolute_import, division, print_function
 
-import os, sys
+import os
+import sys
 
 from setuptools import setup, find_packages
 
@@ -15,33 +16,28 @@ sys.path.insert(0, src_dir)
 import incremental
 
 setup(
-    name='incremental',
+    name="incremental",
     version=incremental.__version__.base(),
-    maintainer='Amber Brown',
-    maintainer_email='hawkowl@twistedmatrix.com',
+    maintainer="Amber Brown",
+    maintainer_email="hawkowl@twistedmatrix.com",
     url="https://github.com/twisted/incremental",
-    classifiers = [
+    classifiers=[
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 2",
-        "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
     ],
     packages=find_packages("src", exclude=("exampleproj",)),
     package_dir={"": "src"},
-    extras_require={
-        "scripts": [
-            "click>=6.0", "twisted>=16.4.0"
-        ]
-    },
+    extras_require={"scripts": ["click>=6.0", "twisted>=16.4.0"]},
     license="MIT",
     zip_safe=False,
-    long_description=open('README.rst').read(),
+    description="A small library that versions your Python projects.",
+    long_description=open("README.rst").read(),
     entry_points="""
     [distutils.setup_keywords]
     use_incremental = incremental:_get_version
